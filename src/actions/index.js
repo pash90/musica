721273378a95bb3662fc757303e3094ce0218bf5
Parth Mehta <tech@iamdifferent.io>
2018-11-11 20:15:42 +1100

    :bug: empty search doesn't make the network request

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,19 @@ const axiosInstance = Axios.create({
 	baseURL: 'https://itunes.apple.com',
 });
 
-export const searchForAlbums = term =>
-	axiosInstance.get(
+export const searchForAlbums = term => {
+	if (!term || term.length === 0) {
+		return Promise.resolve({
+			data: {
+				results: [],
+			},
+		});
+	}
+
+	return axiosInstance.get(
 		`search?term=${term.replace(new RegExp(' ', 'g'), '+')}&entity=album`
 	);
+};
 
 export const searchForSongs = albumId =>
 	axiosInstance.get(`lookup?id=${albumId}&entity=song`);
